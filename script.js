b8f9d98dbd0107de11fdef5aea1f9a4936947ca1
Sarabenitez18 <139276796+Sarabenitez18@users.noreply.github.com>
2023-07-17 15:13:57 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const FLUJO = document.getElementById("flu");
+const BOTON = document.getElementById("Calcular");
+const INPUT = document.getElementById("peso");
+
+BOTON.addEventListener("click", calcular)
+
+function calcular(){
+    console.log("vacio")
+    
+
+console.log (INPUT.value)
+FLUJO.innerHTML = INPUT.value+ "cc/hr";
+FLUJO.style.display = "block"
+}
+
+function holly (peso){
+    let hidratacion
+    
+}
+function superficieCorporal (peso){
+
+}
